be1822ac18815eef45ffb47c061280407e97dec1
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-09-28 12:05:25 +0100

    Fixed code for promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,6 @@
-function getResponseFromAPI(success) {
+export default function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) resolve({ status: 200, body: 'Success' });
     reject(Error('The API is not working correctly'));
   });
 }
-
-export default getResponseFromAPI;
