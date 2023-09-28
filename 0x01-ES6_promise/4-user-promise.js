9d65456dbe69b909c2e5d8a32f5a22ee09917d42
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-09-28 13:54:19 +0100

    Worked on more promises exercises

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -2,8 +2,5 @@ export default function signUpUser(firstName, lastName) {
   return Promise.resolve({
     firstName: firstName,
     lastName: lastName
-    //same as (different methods of writing the same thing)
-    //firstName,
-    //lastName
   });
 }
