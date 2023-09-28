2c7e1e898dc5fc4ee5613f1cf24d83ab0f68f677
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-09-28 11:54:20 +0100

    Promises in JavaScript!

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve,reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    reject(Error('The API is not working correctly'));
+  });
+}
