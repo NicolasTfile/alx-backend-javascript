c8addccd2bc9bbd24522d611bbb16043cb1320fa
Agunloye Olaniyi <nicolastfile@gmail.com>
2023-11-24 07:26:11 +0100

    Made corrections on unit testing in js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,26 +1,11 @@
-const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function () {
-  it('should be 1 + 3 = 4', function () {
-    assert.equal(calculateNumber(1, 3), 4);
-  });
-  it('should be 1 + 3.7 = 5', function () {
-    assert.equal(calculateNumber(1, 3.7), 5);
-  });
-  it('should be 1.2 + 3.7 = 5', function () {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-  });
-  it('should be 1.5 + 3.7 = 6', function () {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-  });
-  it('should be -2 + 4 = 2', function () {
-    assert.equal(calculateNumber(-2, 4), 2);
-  });
-  it('should be 2.6 + -4.7 = -2', function () {
-    assert.equal(calculateNumber(2.6, -4.7), -2);
-  });
-  it('should be -2.5 + -7.5 = -9', function () {
-    assert.equal(calculateNumber(-2.5, -7.5), -9);
+  it('should return the sum of rounded numbers', function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
